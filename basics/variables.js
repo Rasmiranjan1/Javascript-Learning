39ad26dc4bf135eafaae2bcc61138c65f2db2753
Rasmiranjan Dalai <102853128+Rasmiranjan1@users.noreply.github.com>
2023-11-06 18:55:01 +0000

    a new file will created name variables.js

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 5555
+let accountEmail = "rasmiranja@gmail.com"
+var accountPassword = "Raja@098"
+accountName = "saving"
+
+// accountId =2 not allowed
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword])
+
+accountEmail =
+accountPassword
